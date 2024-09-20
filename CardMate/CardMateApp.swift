12d4af2568bf036130b1b0ad13e92e73d590b5cc
Anthony Candelino <anthcandelino@gmail.com>
2024-09-19 21:47:00 -0400

    Initial Commit

---
+++ CardMate/CardMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardMateApp.swift
+//  CardMate
+//
+//  Created by Anthony Candelino on 2024-09-19.
+//
+
+import SwiftUI
+
+@main
+struct CardMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
